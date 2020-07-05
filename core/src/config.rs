24a031022daff872ce4a7d3195a13b0cb31fa694
Mario Ortiz Manero <marioortizmanero@gmail.com>
2020-07-06 00:35:13 +0200

    added argument parsing and better organization

--- core/src/config.rs
+++ core/src/config.rs
@@ -1,37 +1,38 @@
 use crate::player::Player;
 use crate::api::API;
 
+use clap::{App, Arg, ArgMatches, crate_authors, crate_version};
 use ini::Ini;
 use dirs;
 use std::fs::File;
+use std::path::{Path, PathBuf};
 
 macro_rules! opt {
-    ($ini: tt, $name: tt, $section: tt, $default: tt) => ({
-        {
-            // Then the config file, falling back to the default value
-            match $ini.get_from(Some($section), $name) {
-                Some(val) => {
-                    val.parse()
-                       .expect(concat!("Could not parse the value of '",
-                                       $name, "' in the config file."))
-                },
-                None => $default
-            }
-        }
-    });
+    ($args: tt, $ini: tt, $type: ty, $name: expr, $section: expr) => {
+        // The arguments have more priority than config file. Once one
+        // of them is obtained, it's parsed.
+        $args.value_of($name).or_else(|| {
+            $ini.get_from(Some($section), $name)
+        }).and_then(|x| {
+            let err_msg = concat!("Could not parse the value of '", $name,
+                                  "' in the config file.");
+            Some(x.parse::<$type>().expect(err_msg))
+        })
+    };
 }
 
-macro_rules! opt_nodef {
-    ($ini: tt, $name: tt, $section: tt) => ({
-        {
-            // Then the config file, falling back to the default value
-            $ini.get_from(Some($section), $name).and_then(|val| {
-                Some(val.parse()
-                        .expect(concat!("Could not parse the value of '",
-                                        $name, "' in the config file.")))
-            })
-        }
-    });
+macro_rules! arg {
+    ($name: expr, $help: expr) => {
+        Arg::with_name($name)
+            .long($name.replace("_", "-").as_str())
+            .help($help)
+    };
+    ($name: expr, $short: expr, $help: expr) => {
+        Arg::with_name($name)
+            .long($name.replace("_", "-").as_str())
+            .short($short)
+            .help($help)
+    };
 }
 
 
@@ -49,8 +50,7 @@ pub struct Config {
     player: Option<Player>,
     audiosync: bool,
     audiosync_calibration: i32,
-    vlc_args: Option<String>,
-    mpv_flags: Option<String>,
+    mpv_flags: String,
     client_id: Option<String>,
     client_secret: Option<String>,
     redirect_uri: String,
@@ -59,36 +59,75 @@ pub struct Config {
 
 impl Config {
     pub fn new() -> Result<Config, Box<dyn std::error::Error>> {
-        // Reading the file from the user's config directory.
-        let mut file = dirs::config_dir()
-            .expect("Could not find user config directory");
-        file.push("vidify");
-        file.push("config.ini");
-        let file_str = file.to_str()
-            .expect("Invalid UTF-8 characters found in the config path");
-
-        // Checking that the config file exists, and creating it otherwise.
-        if !file.as_path().exists() {
-            File::create(&file).expect("Could not create config file");
-            println!("Created config file at {}", file_str);
-        }
-        let ini = Ini::load_from_file(file_str).unwrap();
+        let args = Config::parse_args();
+        let file = Config::parse_config_file(args.value_of("config_file"));
 
         Ok(Config {
-            debug: opt!(ini, "debug", "Defaults", true),
-            lyrics: opt!(ini, "lyrics", "Defaults", true),
-            fullscreen: opt!(ini, "fullscreen", "Defaults", false),
-            stay_on_top: opt!(ini, "stay_on_top", "Defaults", false),
-            api: opt_nodef!(ini, "api", "Defaults"),
-            player: opt_nodef!(ini, "player", "Defaults"),
-            audiosync: opt!(ini, "audiosync", "Defaults", true),
-            audiosync_calibration: opt!(ini, "audiosync_calibration", "Defaults", 0),
-            vlc_args: opt_nodef!(ini, "vlc_args", "Defaults"),
-            mpv_flags: opt_nodef!(ini, "mpv_flags", "Defaults"),
-            client_id: opt_nodef!(ini, "client_id", "SpotifyWeb"),
-            client_secret: opt_nodef!(ini, "client_secret", "SpotifyWeb"),
-            redirect_uri: opt!(ini, "redirect_uri", "SpotifyWeb", (String::from("http://localhost:8888/callback/"))),
-            refresh_token: opt_nodef!(ini, "refresh_token", "SpotifyWeb"),
+            debug: opt!(args, file, bool, "debug", "Defaults").unwrap_or(true),
+            lyrics: opt!(args, file, bool, "no_lyrics", "Defaults").and_then(|x| Some(!x)).unwrap_or(true),
+            fullscreen: opt!(args, file, bool, "fullscreen", "Defaults").unwrap_or(false),
+            stay_on_top: opt!(args, file, bool, "stay_on_top", "Defaults").unwrap_or(false),
+            api: opt!(args, file, API, "api", "Defaults"),
+            player: opt!(args, file, Player, "player", "Defaults"),
+            audiosync: opt!(args, file, bool, "audiosync", "Defaults").unwrap_or(true),
+            audiosync_calibration: opt!(args, file, i32, "audiosync_calibration", "Defaults").unwrap_or(0),
+            mpv_flags: opt!(args, file, String, "mpv_flags", "Defaults").unwrap_or(String::from("")),
+            client_id: opt!(args, file, String, "client_id", "SpotifyWeb"),
+            client_secret: opt!(args, file, String, "client_secret", "SpotifyWeb"),
+            redirect_uri: opt!(args, file, String, "redirect_uri", "SpotifyWeb").unwrap_or(String::from("")),
+            refresh_token: opt!(args, file, String, "refresh_token", "SpotifyWeb"),
         })
+    }
+
+    fn parse_config_file(path_str: Option<&str>) -> Ini {
+        // The path is first obtained as a `String` for output and 
+        let path = match path_str {
+            Some(path) => PathBuf::from(path),
+            None => {
+                let mut path = dirs::config_dir()
+                    .expect("Could not find user config directory");
+                path.extend(["vidify", "config.ini"].iter());
+                path
+            }
+        };
+
+        // Checking that the config file exists, and creating it otherwise.
+        if !path.exists() {
+            File::create(&path).expect("Could not create config file");
+            println!("Created config file at {}", path.to_str()
+                .expect("Invalid UTF-8 characters found in the config path"));
+        }
+        Ini::load_from_file(path).unwrap()
+    }
+
+    fn parse_args<'a>() -> ArgMatches<'a> {
+        // Basic information about the program.
+        let app = App::new("vidify")
+            .version(crate_version!())
+            .author(crate_authors!());
+
+        // All the available options as arguments.
+        app.args(&[
+            arg!("debug", "d", "display debug messages."),
+            arg!("config_file", "the config file path."),
+            arg!("no_lyrics", "n", "do not print lyrics."),
+            arg!("fullscreen", "f", "open the app in fullscreen mode."),
+            arg!("stay_on_top", "the window will stay on top of all apps."),
+            arg!("api", "a", "the source music player used. Read the \
+                 installation guide for a list with the available APIs."),
+            arg!("player", "p", "the output video player. Read the \
+                 installation guide for a list with the available players."),
+            arg!("audiosync", "enable automatic audio synchronization. Read \
+                 the installation guide for more information. Note: this \
+                 feature is still in development."),
+            arg!("audiosync_callibration", "manual tweaking value for \
+                 audiosync in milliseconds."),
+            arg!("mpv_flags", "custom boolean flags used when opening mpv, \
+                 with dashes and separated by spaces."),
+            arg!("client_id", "the client ID for the Spotify Web API. Check \
+                 the guide to learn how to obtain yours."),
+            arg!("client_secret", "the client secret for the Spotify Web \
+                 API. Check the install guide to learn how to obtain yours."),
+        ]).get_matches()
     }
 }
--- gui/src/main.rs
+++ gui/src/main.rs
@@ -4,7 +4,7 @@ use core::config::Config;
 fn main() {
     let which: API = API::MPRIS;
     let api = init_api(which);
-    println!("Player name: {}", api.get_player_name());
+    // println!("Player name: {}", api.get_player_name());
     let conf = Config::new().unwrap();
     println!("Config: {:?}", conf);
 }
